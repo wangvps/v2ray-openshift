97e687691f8fb85434dda2cfe91f4b7be3d379b8
wangvps <99784662+wangvps@users.noreply.github.com>
2022-07-17 22:57:16 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,7 +1,46 @@
 #!/bin/sh
 # V2Ray new configuration
 cat <<-EOF > /etc/v2ray/config.json
-$CONFIG_JSON
+
+{
+  "inbounds": [{
+    "port": 6899,           
+    "listen":"127.0.0.1",
+    "protocol": "vmess",
+    "settings": {
+      "clients": [
+        {
+          "id": "ee2f35c2-750c-4aab-99b4-45c0bf683caa", 
+          "level": 1,
+          "alterId": 0
+        }
+      ]
+    },
+    "streamSettings": {
+      "network": "ws",
+      "wsSettings": {
+        "path": "/laowang"   
+      }
+    }
+  }],
+  "outbounds": [{
+    "protocol": "freedom",
+    "settings": {}
+  },{
+    "protocol": "blackhole",
+    "settings": {},
+    "tag": "blocked"
+  }],
+  "routing": {
+    "rules": [
+      {
+        "type": "field",
+        "ip": ["geoip:private"],
+        "outboundTag": "blocked"
+      }
+    ]
+  }
+}
 EOF
 # Run V2Ray
 /usr/bin/v2ray/v2ray -config=/etc/v2ray/config.json
